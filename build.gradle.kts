78764d5345b1d4183a659f644b77ecf9a4bf42d0
dc <44590086@qq.com>
2021-06-22 16:55:36 +0800

    modified some file and path

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    id("road-extraction")
+}
+
+group = "com.myproject"
+version = "1.0-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+val kotlinVer = "1.2.10"
+
 dependencies {
-    implementation(project(":common"))
     implementation("com.oracle:ojdbc6:11.2.0.3")
-    implementation("org.elasticsearch:elasticsearch-spark-20_2.11:6.3.0")
     implementation("junit:junit:4.12")
     compileOnly("org.apache.spark:spark-core_2.11:2.2.0.2.6.3.0-235")
     compileOnly("org.apache.spark:spark-streaming_2.11:2.2.0.2.6.3.0-235")
@@ -9,5 +24,10 @@ dependencies {
     compileOnly("org.apache.spark:spark-streaming-kafka-0-10_2.11:2.2.0.2.6.3.0-235")
     compileOnly("org.apache.spark:spark-hive_2.11:2.2.0")
     implementation("com.typesafe:config:1.3.3")
-
+}
+compileKotlin {
+    kotlinOptions.jvmTarget = "1.8"
+}
+compileTestKotlin {
+    kotlinOptions.jvmTarget = "1.8"
 }
